e9718e9c857ca5bf534d5972c1a1dcd18bfe35b3
Rahul Solanki <rsolanki@csod.com>
2020-07-27 20:36:56 +0530

    Updates

---
+++ precache-manifest.44eca5941ae38e6e5c2cb08e95076e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7279e34afc60e51083de3b1dcec1c498",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c2ad0b53dbc0fc27dafc",
+    "url": "/monsters-rolodex/static/css/main.1bab6a66.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ad0b53dbc0fc27dafc",
+    "url": "/monsters-rolodex/static/js/main.54206a65.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
